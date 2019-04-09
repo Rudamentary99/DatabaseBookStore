68e63d0e844d80a54f18a02b98e943bc6d4ce71c
Rudamentary99 <49048346+Rudamentary99@users.noreply.github.com>
2019-04-09 11:32:06 -0500

    Add files via upload

--- SheeteMue/src/Servlet/SheeteMueServlet.java
+++ SheeteMue/src/Servlet/SheeteMueServlet.java
@@ -1,6 +1,9 @@
 package Servlet;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,33 +18,41 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SheetieServlet")
 public class SheeteMueServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SheeteMueServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+	public SheeteMueServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
 			throws ServletException, IOException {
-		if (request.getServletPath().equals("/SheetieServlet")) {
-			RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/jsp/index.jsp");
+		String action =  request.getParameter("action");
+		System.out.println(action);
+		if (action != null && action.equals("SaveFile")) {
+			//TODO: WRITE o TO DISK SOMEHOW
+			Object o = request.getParameter("img");
+			System.out.println((File) o);
+			RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/jsp/itemCreationSuccess.jsp");
 			requestDispatcher.forward(request, response);
 		} else {
-
-			System.out.println(request.getServletPath());
+			RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/jsp/index.jsp");
+			requestDispatcher.forward(request, response);
 		}
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
